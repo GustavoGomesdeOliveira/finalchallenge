2a78fb675e95a69c7000e695d846ce70a2a6260c
bergmpe <bergmpe@gmail.com>
2017-05-08 09:49:19 -0300

    it added this app to firebase project.

--- FinalChallenge/FinalChallenge/AppDelegate.swift
+++ FinalChallenge/FinalChallenge/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 import FBSDKCoreKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         // Add any custom logic here.
+        FIRApp.configure()
         return true
     }
     
